e62b24c73d00386dc732faa5dd9303710848e343
Luckydipper <qhrqufdlek@naver.com>
2020-10-25 21:51:17 +0900

    add button

--- _GUI.py
+++ _GUI.py
@@ -9,10 +9,7 @@ import sys
 from PyQt5 import QtCore
 from PyQt5 import QtWidgets
 from PyQt5 import QtGui
-
 from PyQt5.QtGui import QIcon
-from pyasn1.compat import binary
-from six import binary_type
 
 
 class ShowVideo(QtCore.QObject):
@@ -23,7 +20,6 @@ class ShowVideo(QtCore.QObject):
     height, width = image.shape[:2] # 사진의 픽셀을 확인함.
 
     VideoSignal1 = QtCore.pyqtSignal(QtGui.QImage)
-    VideoSignal2 = QtCore.pyqtSignal(QtGui.QImage)
 
     def __init__(self, parent=None):
         super(ShowVideo, self).__init__(parent)
@@ -37,16 +33,13 @@ class ShowVideo(QtCore.QObject):
         while run_video:
             ret, image = self.camera.read()
             color_swapped_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
+            
             qt_image1 = QtGui.QImage(color_swapped_image.data, # ndarray data attribute 버퍼객체의 시작을 가르킨다는데 잘 모르겠음.
                                     self.width,
                                     self.height,
                                     color_swapped_image.strides[0],
                                     QtGui.QImage.Format_RGB888)
             self.VideoSignal1.emit(qt_image1)
-            
-            self._senseCapture()
-
             loop = QtCore.QEventLoop()
             QtCore.QTimer.singleShot(25, loop.quit) #25 ms
             loop.exec_()
@@ -75,7 +68,7 @@ class ImageViewer(QtWidgets.QWidget):
         self.setWindowTitle('CITIZEN PROJ')
         self.setWindowIcon(QIcon('./resource/images/networking.png'))
         self.move(300,300)
-        self.resize(1000,800)
+        self.resize(500,800)
     
     @QtCore.pyqtSlot(QtGui.QImage)
     def setImage(self, image):
@@ -87,6 +80,8 @@ class ImageViewer(QtWidgets.QWidget):
             self.setFixedSize(image.size())
         self.update()
 
+def say_hello():
+    print("button clicked, Hello")
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
@@ -95,12 +90,11 @@ if __name__ == '__main__':
     thread = QtCore.QThread()
     thread.start()
 
-
     vid = ShowVideo()
     vid.moveToThread(thread)
 
     image_viewer1 = ImageViewer()
-
+    
     vid.VideoSignal1.connect(image_viewer1.setImage)
 
     push_button1 = QtWidgets.QPushButton('Start Video')
@@ -112,9 +106,15 @@ if __name__ == '__main__':
 
     vertical_layout = QtWidgets.QVBoxLayout()
     horizontal_layout = QtWidgets.QHBoxLayout()
-    horizontal_layout.addWidget(image_viewer1)
+
+    
+    push_button1.clicked.connect(say_hello)
+
     vertical_layout.addLayout(horizontal_layout)
     vertical_layout.addWidget(push_button1)
+    vertical_layout.addWidget(push_button2)
+    vertical_layout.addWidget(push_button3)
+    horizontal_layout.addWidget(image_viewer1)
 
     layout_widget = QtWidgets.QWidget()
     layout_widget.setLayout(vertical_layout)
